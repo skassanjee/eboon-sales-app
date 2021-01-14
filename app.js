55f39839fab15feca75e1b6b8ae5c131d8c2ac20
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-01-14 04:44:03 -0700

    finished project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+const keys = require('./config/keys')
+const stripe = require('stripe')(keys.stripeSecretKey)
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars');
 
@@ -16,11 +17,34 @@ app.use(bodyParser.urlencoded({extended: false}));
 //set static folder
 app.use(express.static(`${__dirname}/public`))
 
+
+//charge route
+
+app.post('/charge', (req, res) => {
+    const amount = 2500
+    // console.log(req.body) -- shows us what is available when we submit the payment form
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+    .then(customer => stripe.charges.create({
+        amount,
+        description: 'web dev ebook',
+        currency: 'usd',
+        customer: customer.id
+    }))
+    .then(charge => res.render('success'));
+})
+
 //index route
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    })
 })
+
+
 
 
 const port = process.env.port || 5000
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports ={
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKet: process.env.STRIPE_SECRET_KEY
+}
