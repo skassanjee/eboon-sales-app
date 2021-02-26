f947c1a1df376d00cdb2b9a2e7236b3e3eaf2932
Sahil <56181733+skassanjee@users.noreply.github.com>
2021-02-25 23:56:28 -0700

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const keys = require('./config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars');
+const request = require('request')
+const path = require('path')
 
 const app = express();
 
@@ -12,7 +14,7 @@ app.set('view engine', 'handlebars');
 
 //body parser middleware
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //set static folder
 app.use(express.static(`${__dirname}/public`))
@@ -44,8 +46,61 @@ app.get('/', (req, res) => {
     })
 })
 
+//newsletter signup route
 
+app.post('/signup', (req,res) => {
+    const {firstName, lastName, email } = req.body
+    
+    //validation
+    if(!firstName || !lastName || !email){
+        console.log('add a redirect')
+        //res.redirect('/fail.html') return
+    }
 
+ 
+    //mailchimp
+
+    //construct req data
+
+    const data ={
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const postData = JSON.stringify(data)
+
+    const options = {
+        url:  'https://us1.api.mailchimp.com/3.0/lists/d778c1f68e',
+        method: 'POST',
+        headers:{
+            Authorization: 'auth 1efa95a431712629b9d2a036baac0c92-us1'
+        },
+        body: postData
+    }
+
+    request(options, (err, response, body) => {
+        if(err){
+            res.redirect('/fail.html')
+            console.log(err)
+        }
+        else{
+            if(response.statusCode === 200){
+                res.redirect('/success.html')
+            }
+            else{
+                res.redirect('/fail.html')
+            }
+        }
+    })
+})
 
 const port = process.env.port || 5000
 
